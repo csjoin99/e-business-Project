df02568b98bdcc8c2c6bc696d064564fe5e859ed
Diego <csjoin99@gmail.com>
2021-10-21 11:31:08 -0500

    logo btn route modified

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('admin.dashboard') }}" class="brand-link">
         {{-- <img src="{{ asset('img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
         style="opacity: .8; width: 50px; height: 50px"> --}}
         <span class="brand-text font-weight-light ml-2">PORTAL COMMERCE</span>
