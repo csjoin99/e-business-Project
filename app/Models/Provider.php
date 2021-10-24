54b64f375262196ee06779c79f8a1205c5ae677a
Diego <csjoin99@gmail.com>
2021-10-24 01:40:39 -0500

    audit for providers added

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Arr;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as AuditableTrait;
 
-class Provider extends Model
+class Provider extends Model implements Auditable
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, AuditableTrait;
 
     protected $table = "provider";
 
@@ -20,8 +23,59 @@ class Provider extends Model
         'email',
     ];
 
+    protected $auditInclude = [
+        'name',
+        'ruc',
+        'address',
+        'phone',
+        'email',
+    ];
+
+    const MODULE_NAME = 'Proveedores';
+
+    const FIELDS = [
+        'name' => [
+            'name' => 'Nombre',
+            'field' => 'input',
+        ],
+        'ruc' => [
+            'name' => 'RUC',
+            'field' => 'input',
+        ],
+        'address' => [
+            'name' => 'Dirección',
+            'field' => 'input',
+        ],
+        'phone' => [
+            'name' => 'Teléfono',
+            'field' => 'input',
+        ],
+        'email' => [
+            'name' => 'Email',
+            'field' => 'input',
+        ],
+    ];
+
     public function getFieldAttribute()
     {
         return $this->name;
+    }
+
+    protected function getUpdatedEventAttributes(): array
+    {
+        $old = [];
+        $new = [];
+
+        foreach ($this->attributes as $attribute => $value) {
+            if ($this->isAttributeAuditable($attribute)) {
+                $old[$attribute] = Arr::get($this->original, $attribute);
+                $new[$attribute] = Arr::get($this->attributes, $attribute);
+            }
+        }
+
+        return [
+            $old,
+            $new,
+        ];
     }
 }
